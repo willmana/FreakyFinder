ce16a048a4f5581d4ed62b3e03ccf76d1eac2b2c
willmana <aleksi.willman@gmail.com>
2022-09-08 16:11:11 +0300

    Add left sidebar with initial links

---
+++ frontend/src/containers/SideBar.js
@@ -0,0 +1,39 @@
+import { useMessageGetter } from '@messageformat/react';
+import React from 'react';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import NewspaperIcon from '@mui/icons-material/Newspaper';
+import PeopleIcon from '@mui/icons-material/People';
+import MessageIcon from '@mui/icons-material/Message';
+import PersonSearchIcon from '@mui/icons-material/PersonSearch';
+import SettingsIcon from '@mui/icons-material/Settings';
+import styles from './SideBar.module.scss';
+
+const SideBar = (props) => {
+    const msg = useMessageGetter('SideBar');
+    return (
+        <div className={styles.buttoncontainer}>
+            <button className={styles.button}>
+                <AccountBoxIcon /> <p>{msg('profile')}</p>
+            </button>
+            <button className={styles.button}>
+                <NewspaperIcon /> <p>{msg('feed')}</p>
+            </button>
+            <button className={styles.button}>
+                <PeopleIcon /> <p>{msg('friends')}</p>
+            </button>
+            <button className={styles.button}>
+                <MessageIcon /> <p>{msg('messages')}</p>
+            </button>
+            <button className={styles.button}>
+                <PersonSearchIcon /> <p>{msg('findContacts')}</p>
+            </button>
+            <button className={styles.button}>
+                <SettingsIcon /> <p>{msg('settings')}</p>
+            </button>
+        </div>
+    );
+};
+
+SideBar.propTypes = {};
+
+export default SideBar;
--- frontend/src/pages/FreakyFinder.js
+++ frontend/src/pages/FreakyFinder.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Feed from '../containers/Feed';
+import SideBar from '../containers/SideBar';
 import styles from './FreakyFinder.module.scss';
 
 const FreakyFinder = () => {
     return (
         <div className={styles.mainpagecontainer}>
-            <div className={styles.leftbar}></div>
+            <div className={styles.leftbar}>
+                <SideBar />
+            </div>
             <div className={styles.middlebar}>
                 <Feed />
             </div>
